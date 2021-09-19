b535d8852a4cf73b7cc9a27434de50a79b05ab92
ms919 <sakuraimiki919@gmail.com>
2021-09-19 12:21:44 +0900

    Add: firebase hosting deploy設定

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,12 @@
 <template>
 	<div>
 		<h1>Topページ</h1>
-    <img src="~/assets/test.png">
 		<router-link to="/users">users</router-link>
-    <router-link to="/new">new</router-link>
+		<router-link to="/new">new</router-link>
 	</div>
 </template>
 <style scoped>
 img {
-  height: 100px;
+	height: 100px;
 }
 </style>
